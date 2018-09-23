ad7ec9f149f2bf929e26b01a72519dec2a0dfcc9
Jasmin Baniya <jbaniya42@students.tntech.edu>
2018-09-23 13:01:30 -0500

    Project Fixed.

---
+++ AgentsManagement/DataAccessLayer/Users/UsersRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Contracts.Users;
+using DataAccessLayer.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Users
+{
+    public class UsersRepository : IUsersRepository
+    {
+        public IEnumerable<User> GetUsers()
+        {
+            return null;
+        }
+    }
+}
